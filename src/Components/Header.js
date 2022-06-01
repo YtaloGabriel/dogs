4af1dc5d9f6f20f0a6c1d961b58cdfc4c4aae095
Ytalo Gabriel <ytalogabr@gmail.com>
2022-06-01 04:33:42 -0300

    Header creation and stylization

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Styles from './css/Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
     <header className={Styles.header}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${Styles.nav} container`}>
+        <Link className={Styles.logo} to="/">
+          <Dogs />
+        </Link>
+        <Link className={Styles.login} to="/login">
+          Entrar / Criar
+        </Link>
       </nav>
     </header>
   );
