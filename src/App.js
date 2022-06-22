8d5a0500be7a30171638c235bf69fbddf7df6d1c
Ytalo Gabriel <ytalogabr@gmail.com>
2022-06-22 06:12:20 -0300

    Created User Profile page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <Route path="account/*" element={<User />} />
             <Route path="photo/:id" element={<Photo />} />
+            <Route path="profile/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
