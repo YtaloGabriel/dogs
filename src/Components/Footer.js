a36ea8c055c8f51c68de2a30101aeda0374b2adb
Ytalo Gabriel <ytalogabr@gmail.com>
2022-06-23 02:16:23 -0300

    Footer style added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Styles from './css/Footer.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs-footer.svg';
 
 const Footer = () => {
-  return <footer className={Styles.footer}></footer>;
+  return (
+    <footer className={Styles.footer}>
+      <Dogs />
+      <p>Dogs - 06/2022. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
