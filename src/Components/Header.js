0f524ace6601573d06751b6256043905a4c0267b
Ytalo Gabriel <ytalogabr@gmail.com>
2022-06-24 02:26:24 -0300

    Settings to gh-pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,15 +10,15 @@ const Header = () => {
   return (
     <header className={Styles.header}>
       <nav className={`${Styles.nav} container`}>
-        <Link className={Styles.logo} to="/">
+        <Link className={Styles.logo} to="/dogs">
           <Dogs />
         </Link>
         {data ? (
-          <Link className={Styles.login} to="/login">
+          <Link className={Styles.login} to="/dogs/login">
             {data.nome}
           </Link>
         ) : (
-          <Link className={Styles.login} to="/login">
+          <Link className={Styles.login} to="/dogs/login">
             Entrar / Criar
           </Link>
         )}
--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -4,7 +4,7 @@ import { UserContext } from '../../UserContext';
 
 const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext);
-  return login ? children : <Navigate to="/login" />;
+  return login ? children : <Navigate to="/dogs/login" />;
 };
 
 export default ProtectedRoute;
