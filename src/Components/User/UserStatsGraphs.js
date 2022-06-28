fc772a10a37a4675d34252129d7195a324542dd0
Ytalo Gabriel <ytalogabr@gmail.com>
2022-06-28 06:18:39 -0300

    Bug fixed on Stats page

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -7,49 +7,57 @@ const UserStatsGraphs = ({ data }) => {
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
-    const graphData = data.map((item) => {
-      return {
-        x: item.title,
-        y: Number(item.acessos),
-      };
-    });
+    if (data.length) {
+      const graphData = data.map((item) => {
+        return {
+          x: item.title,
+          y: Number(item.acessos),
+        };
+      });
 
-    setTotal(
-      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
-    );
+      setTotal(
+        data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
+      );
 
-    setGraph(graphData);
+      setGraph(graphData);
+    }
   }, [data]);
 
   if (data) {
     return (
       <section className={`${Styles.graph} animeLeft`}>
         <div className={`${Styles.total} ${Styles.graphItem}`}>
-          <p>Acessos: {total}</p>
+          <p>
+            {data.length ? `Acessos: ${total}` : `Você ainda não possui fotos`}
+          </p>
         </div>
-        <div className={Styles.graphItem}>
-          <VictoryPie
-            data={graph}
-            innerRadius={50}
-            padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
-            style={{
-              data: {
-                fillOpacity: 0.9,
-                stroke: '#fff',
-                strokeWidth: 2,
-              },
-              labels: {
-                fontSize: 14,
-                fill: '#333',
-              },
-            }}
-          />
-        </div>
-        <div className={Styles.graphItem}>
-          <VictoryChart>
-            <VictoryBar alignment="start" data={graph} />
-          </VictoryChart>
-        </div>
+        {data.length && (
+          <>
+            <div className={Styles.graphItem}>
+              <VictoryPie
+                data={graph}
+                innerRadius={50}
+                padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+                style={{
+                  data: {
+                    fillOpacity: 0.9,
+                    stroke: '#fff',
+                    strokeWidth: 2,
+                  },
+                  labels: {
+                    fontSize: 14,
+                    fill: '#333',
+                  },
+                }}
+              />
+            </div>
+            <div className={Styles.graphItem}>
+              <VictoryChart>
+                <VictoryBar alignment="start" data={graph} />
+              </VictoryChart>
+            </div>
+          </>
+        )}
       </section>
     );
   } else {
